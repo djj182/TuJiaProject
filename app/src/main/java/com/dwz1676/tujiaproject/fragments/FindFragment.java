143c5cdda2da3abf69fb6ac4d4c44908523ffde9
WeiYaFei <1074146921@qq.com>
2016-07-07 16:13:54 +0800

    dddde3eeee

--- app/src/main/java/com/dwz1676/tujiaproject/fragments/FindFragment.java
+++ app/src/main/java/com/dwz1676/tujiaproject/fragments/FindFragment.java
@@ -3,6 +3,7 @@ package com.dwz1676.tujiaproject.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
