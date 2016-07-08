6674af4c6a752f830a689840b346dc9cf746890c
hm-chou <1157971793@qq.com>
2016-07-08 19:31:40 +0800

    aaaaaa

---
+++ app/src/main/java/com/dwz1676/tujiaproject/activities/MoreActivity.java
@@ -0,0 +1,23 @@
+package com.dwz1676.tujiaproject.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.dwz1676.tujiaproject.R;
+
+/**
+ * Created by Administrator on 16-7-8.
+ */
+public class MoreActivity extends AppCompatActivity {
+
+    private ImageView imageview_back;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_activity);
+        imageview_back = (ImageView) findViewById(R.id.imageview_back);
+    }
+}
--- app/src/main/java/com/dwz1676/tujiaproject/fragments/MyInfoFragment.java
+++ app/src/main/java/com/dwz1676/tujiaproject/fragments/MyInfoFragment.java
@@ -1,18 +1,25 @@
 package com.dwz1676.tujiaproject.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.dwz1676.tujiaproject.R;
+import com.dwz1676.tujiaproject.activities.MoreActivity;
 
 /**
  * Created by Administrator on 2016/7/6.
  */
 public class MyInfoFragment extends Fragment {
+
+    private View view;
+    private ImageView imageview_setting;
+    private ImageView imageview_setting1;
 
     public MyInfoFragment() {
     }
@@ -27,7 +34,15 @@ public class MyInfoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.my_info,null);
+        View view = inflater.inflate(R.layout.my_info_fragment,null);
+        imageview_setting = (ImageView)view.findViewById(R.id.imageview_setting);
+        imageview_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), MoreActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
