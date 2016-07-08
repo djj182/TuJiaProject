97e6fdbce1df5648ca1dd6d2e5154438e7c40020
hm-chou <1157971793@qq.com>
2016-07-08 12:06:35 +0800

    aaaaaa

--- app/src/main/java/com/dwz1676/tujiaproject/fragments/MyInfoFragment.java
+++ app/src/main/java/com/dwz1676/tujiaproject/fragments/MyInfoFragment.java
@@ -7,22 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.dwz1676.tujiaproject.R;
+
 /**
  * Created by Administrator on 2016/7/6.
  */
 public class MyInfoFragment extends Fragment {
+
     public MyInfoFragment() {
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.my_info,null);
+        return view;
     }
 
     @Override
